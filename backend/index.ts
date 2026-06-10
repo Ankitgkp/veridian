3a7a4f12528355bb40ebad939ee999858ec4325f
Ankit Pandey <36405347+Ankitgkp@users.noreply.github.com>
2026-06-10 23:46:11 +0530

    INit

---
+++ backend/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+
+
+app.post('/converstation', async (req, res) => {
+    
+})
+
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
